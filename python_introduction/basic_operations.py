c4cb5682fc55799b65e09e573b10e78520e48b83
Gabriel Martey <gabrielmart400@gmail.com>
2025-09-18 17:27:53 +0000

    Added files to a directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+# addition
+sum = number1 + number2
+
+# subtract
+sub = number1 - number2
+
+# multipllcation
+mul = number1 * number2
+
+# display
+print(f"Addition of {number1} + {number2} is {sum}")
+print(f"Subtraction of {number1} - {number2} is {sub}")
+print(f"Multiplication of {number1} * {number2} is {mul}")
